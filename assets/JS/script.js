b3e9ee2f1647bee7091df47d05bd4816ff489832
Fellipe Santos <fellipeunker12@gmail.com>
2023-12-19 14:26:30 -0300

    detalhes finalizados faltando apenas responsividade na página

---
+++ assets/JS/script.js
@@ -0,0 +1,13 @@
+function salvaEmail () {
+    const inputEmail = document.getElementById('email');
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        alert('Email Salvo com sucesso!');
+        console.log({
+            email: inputEmail.value,
+        });
+    })
+}
+salvaEmail();
